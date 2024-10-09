99b33bd368891a98862dd0573d4df0657f917cc6
KYs <amitkys59@gmail.com>
2024-10-09 14:46:43 +0530

    added script file

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function App() {
     <Table aria-label="Example static collection table">
       <TableHeader>
         <TableColumn>NAME</TableColumn>
-        <TableColumn>ROLE</TableColumn>
+        <TableColumn className="text-center">ROLE</TableColumn>
         <TableColumn>STATUS</TableColumn>
       </TableHeader>
       <TableBody>
         <TableRow key="1">
           <TableCell className="w-5">8 Jun</TableCell>
-          <TableCell>uiyiyiy</TableCell>
+          <TableCell className="text-center">uiyiyiy</TableCell>
           <TableCell className="w-[70px]"><div className="flex flex-row">
            <Link className="mr-1"  href={''}><img className="h-10 w-10" src="/viewIcon.svg" alt="" /></Link>
            <Link href={''}><img className="h-6 w-6 mt-2" src="/deleteIcon.svg" alt="" /></Link>
